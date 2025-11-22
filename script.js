945778bd5952fab0969363993b5bb1f3f0b99846
Marcielli Oliveira <marciellileticiaol@gmail.com>
2025-11-22 14:14:30 -0300

    Lógica implementada da função filtrarPorCategoria()

--- script.js
+++ script.js
@@ -64,3 +64,24 @@ function filtrarPesquisa() {
         video.style.display = "block";
     }
 }
+
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos) {
+        let categoria = video.querySelector(".categoria").textContent.toLocaleLowerCase();
+        let valorFiltro = filtro.toLocaleLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
