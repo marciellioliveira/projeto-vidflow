07e9fd19a4a0fa92d6cd481668dd2dfdab9f45ee
Marcielli Oliveira <marciellileticiaol@gmail.com>
2025-11-22 14:06:34 -0300

    Lógica da barra de pesquisa

--- script.js
+++ script.js
@@ -41,3 +41,26 @@ async function buscarEMostrarVideos() {
 }
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item");
+
+    if(barraDePesquisa.value != "") {
+        for(let video of videos) {
+            let titulo = video.querySelector(".titulo-video").textContent.toLocaleLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLocaleLowerCase();
+
+            if(!titulo.includes(valorFiltro)) {
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+        }
+    } else {
+        video.style.display = "block";
+    }
+}
